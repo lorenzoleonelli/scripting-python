f7aaad9fe44c97bdcfb4761d8844e723af5c8dcb
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-11 17:19:48 +0100

    2022.02.11 v2

--- full_tutorial.py
+++ full_tutorial.py
@@ -1,1 +1,14 @@
-print("hello world")
+## Staff from tutorial The Complete Python Bootcamp From Zero to Hero in Python (Udemy)
+## Comments are doubled
+
+## S3.11 Introduction to Python Data Types
+## NAME             TYPE        DESCRIPTION
+## Integers         int
+## Floating Point   float
+## Strings          str         Ordered Sequence of Characters
+## Lists            list        Ordered Sequence of Objects
+## Dictionaries     dict        Unordered Key:Value pair
+## Tuples           tup         Ordered non mutable sequence of objects
+## Sets             set         Unordered Collection of Unique Objects
+## Booleans         bool
+
