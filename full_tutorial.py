56b0fbbd0fce1bd5e6f489dbc5d97ffae9e847b1
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-12 18:37:06 +0100

    20213.02.12

--- full_tutorial.py
+++ full_tutorial.py
@@ -11,4 +11,25 @@
 ## Tuples           tup         Ordered non mutable sequence of objects
 ## Sets             set         Unordered Collection of Unique Objects
 ## Booleans         bool
+##
+
+## S3.11 Remember floating point accuracy and computer's abilities to represent numbers in memory
+# print (0.1+0.2-0.3)
+
+## S3.14 Variable Assignement
+## Use lower case, do not use special character , do not use word with special meaning (ex: list, str)
+## Dynamic Typing: you can reassign variables to different Data Types
+## type() is helpful to identify what data type you are using
+## variable assignement: a = 5
+## variable reassignement: a = a + 10
+
+## S3.15 Strings
+## Ordered Sequences of characters using " or ' -> can be indexed/sliced
+## slicing = [start:stop:step]
+# print("hello") 
+# print ("hello \nworld")
+# print ("hello \tworld")
+## len() gives the length of a string
+#print(len("ciao"))
+
 
