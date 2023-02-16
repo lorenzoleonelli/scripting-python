d97dc8ca330f9793b4c3328a9b4e12edbc82c3e6
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-16 21:01:35 +0100

    2022.02.16

--- full_tutorial.py
+++ full_tutorial.py
@@ -254,4 +254,31 @@
 #for item in d.values():
 #    print(item)
 
-## 5.35 For Loops
+## 5.36 While Loops
+
+#x = 0
+#while x<5:
+#    print(f"value of x is {x}")
+#    x += 1
+#else:
+#    print("x is not less than 5")
+
+## break: breaks out of the current closest enclosing loop
+## continue: goes to the top of the closest enclosing loop
+## pass: do nothing
+
+#x=[1,2,3]
+#for item in x:
+#    pass
+
+#mystring="ciao"
+#for letter in mystring:
+#    if letter == "a":
+#        continue
+#    print(letter)
+
+mystring="ciao"
+for letter in mystring:
+    if letter == "a":
+        break
+    print(letter)
