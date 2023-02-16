8aca2aab003c0f90f5be068ea463f6cfa73998e9
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-16 20:36:08 +0100

    2022.02.16

--- full_tutorial.py
+++ full_tutorial.py
@@ -232,8 +232,26 @@
 #for letter in my_string:
 #    print(letter)
 
-for letter in "Hello World":
-    print(letter)
+#for letter in "Hello World":
+#    print(letter)
 
+#tup=(1,2,3)
+#for item in tup:
+#    print(item)
+
+#mylist = [(1,2),(3,4),(5,6),(7,8)]
+#len(mylist)
+#for item in mylist:
+#    print(item)
+#for (a,b) in mylist: #tuple unpucking
+#    print(b)
+
+#d={'k1':1, 'k2':2, 'k3':3}
+#for item in d:
+#    print(item)
+#for item in d.items():
+#    print(item)
+#for item in d.values():
+#    print(item)
 
 ## 5.35 For Loops
