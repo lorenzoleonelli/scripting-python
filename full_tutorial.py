bb710106bd2dba9dc196e9320c44c0a5d9c1887b
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-20 21:15:52 +0100

    2022.02.20

--- full_tutorial.py
+++ full_tutorial.py
@@ -406,9 +406,46 @@
 
 ## 6.46 Tuple unpacking with functions
 
+#work_hours = [("Abby",100),("Billy",2000),("Cassie",400)]
 
+#def emp_chk(work_hours):
+#    curr_max = 0
+#    emp_of_month = ""
+#    for employee,hours in work_hours:
+#        if hours > curr_max:
+#            curr_max = hours
+#            emp_of_month = employee
+#        else:
+#            pass
+#    return(emp_of_month,curr_max)
+
+#print(emp_chk(work_hours))
 
 ## 6.47 Interaction between functions
+
+""" from random import shuffle
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+def player_guess():
+    guess=""
+    while guess not in["0","1","2"]:
+        guess = input("Pick a number: 0,1, or 2: ")
+    return int(guess)
+
+def check_guess(mylist,guess):
+    if mylist[guess] == "0":
+        print("Correct!")
+    else:
+        print("Wrong!")
+        print(mylist)
+
+mylist = [" ", "O", " "]
+mixed_list = shuffle_list(mylist)
+guess = player_guess()
+check_guess(mixed_list,guess) """
 
 
 
