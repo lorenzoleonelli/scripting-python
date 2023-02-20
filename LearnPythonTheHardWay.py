70cb2a0dcda3ebcf97e22710be3da4748e16a08f
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-20 11:35:58 +0100

    2022.02.20

--- LearnPythonTheHardWay.py
+++ LearnPythonTheHardWay.py
@@ -2,13 +2,40 @@
 
 ## EX1: A good First Program
 
-print ("Hello World!")
-print ("Hello again")
-print ("I like typing this")
-print ("This is fun")
-print ('Yay! Printing.')
-print ("I'd much rather you 'not'")
-print ('I "said" do not touch this.')
+#print ("Hello World!")
+#print ("Hello again")
+#print ("I like typing this")
+#print ("This is fun")
+#print ('Yay! Printing.')
+#print ("I'd much rather you 'not'")
+#print ('I "said" do not touch this.')
 
 ## EX2: Comments and Pound Characters
+
+# A comment, this is so you can read your program l8r
+# Anything after the # is ignored
+
+# print("I can have code like this") #And all after this is ignored
+
+# print("This won't run")
+# print("This will run")
+
+## EX3: Numbers and math
+## Order of Operations is PEMDAS: Parentheses, Exponents, Multiplication, Division, Addition, Subtraction 
+
+#print("I will now count my chickens")
+#print("Hens", 25 + 30/6)
+#print("Roosters", 100 - 25 * 3 % 4)
+#print("Now I will count the eggs")
+#print(3+2+1-5+4%2-1/4+6)
+#print(3+2<5-7)
+#print("What is 3+2", 3+2)
+#print(5>-2)
+#print(5>=-2)
+#print(5<=-2)
+
+## EX4: Variables and Names
+
+
+
 
