2d48d5af2d6478c71638f09cbed61f0bc200d02d
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-20 10:53:29 +0100

    2023.02.20

--- LearnPythonTheHardWay.py
+++ LearnPythonTheHardWay.py
@@ -1,3 +1,11 @@
 ## Exercisec from Book: Zed Shaw "Learn Python the Hard Way", 3rd Edition
 
 ## EX1: A good First Program
+
+print "Hello World!"
+print "Hello again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this.'
