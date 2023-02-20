39752ce6f546c1ac91aadcd629936fc0a392d73e
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-20 16:51:31 +0100

    2022.02.20

--- LearnPythonTheHardWay.py
+++ LearnPythonTheHardWay.py
@@ -70,7 +70,52 @@ print("His teeth are generalli %s depending on the cofee" % my_teeth) """
 
 ## EX6: Strings and Text
 
+""" x = "There are %d types of people" % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s" % (binary, do_not)
 
+print(x)
+print(y)
+
+print ("I said: %r" % x)
+print (" I also said: %s'." %y)
+
+hilarious = False
+joke_eval = "Isn't that joke so funnny ? %r"
+
+print (joke_eval % hilarious)
+
+w ="Left side of ..."
+e ="a string"
+
+print (w + e) """
+
+## EX7: More Printing
+
+""" print ("Mary had a little lamb.")
+print ("Its fleece was white as %s." % 'snow')
+print ("And everywhere that Mary went.")
+print ("." * 10) # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+ # watch that comma at the end. try removing it to see what happens
+print (end1 + end2 + end3 + end4 + end5 + end6,)
+print (end7 + end8 + end9 + end10 + end11 + end12) """
+
+##
 
 
 
