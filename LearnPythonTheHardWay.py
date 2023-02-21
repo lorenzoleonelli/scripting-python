b3a2603de2d5c35b8b7fa22b93fff2cac726fbcc
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-21 08:03:29 +0100

    2022.02.21

--- LearnPythonTheHardWay.py
+++ LearnPythonTheHardWay.py
@@ -133,6 +133,40 @@ print (formatter % (
 
 ## EX 9: Printing, Printing, Printing
 
+#days = "Mon Tue Wed Thu Fri Sat Sun"
+#months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul"
+
+#print("Here are the days", days)
+#print("Here are the months:", months)
+#print ("""
+#there is something here
+#with the 3 double quotes
+#we can type sooo much
+#""")
+
+
+## EX 10: What was that
+
+#tabby_cat = "\tI'm tabbed in."
+#persian_cat = "I'm split\non a line."
+#backslash_cat = "I'm \\ a \\ cat."
+
+#fat_cat = """
+#I'll do a list:
+#\t* Cat food
+#\t* Fishies
+#\t* Catnip\n\t* Grass
+#"""
+
+#print (tabby_cat)
+#print (persian_cat)
+#print (backslash_cat)
+#print (fat_cat)
+
+## EX 11: Asking Questions
+
+""" age = input("How old are you ?")
+print("Hello, you are %r old" % age) """
 
 
 
