312ed1252dfa84d53ad4b29910e6a4d70489f7ac
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-22 16:25:17 +0100

    2023.02.22

--- CodingBatPython.py
+++ CodingBatPython.py
@@ -51,13 +51,68 @@
 ## diff21(10) → 11
 ## diff21(21) → 0
 
-def diff21(n):
-    if n > 21:
-        return 2 * (n-21)
-    else:
-        return abs(n-21)
+#def diff21(n):
+#    if n > 21:
+#        return 2 * (n-21)
+#    else:
+#        return abs(n-21)
 
-print(diff21(19))
-print(diff21(10))
-print(diff21(21))      
+#print(diff21(19))
+#print(diff21(10))
+#print(diff21(21))
+ 
+
+## We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.       
+##parrot_trouble(True, 6) → True
+##parrot_trouble(True, 7) → False
+##parrot_trouble(False, 6) → False
+
+#def parrot_trouble(talking, hour):
+#    if talking == True and (hour < 7 or hour > 20):
+#        return True
+#    else:
+#        return False
+
+#print(parrot_trouble(True, 6))
+#print(parrot_trouble(True, 7))
+#print(parrot_trouble(False, 6))
+
+## Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+## makes10(9, 10) → True
+## makes10(9, 9) → False
+## makes10(1, 9) → True
+
+#def makes10(a, b):
+#    if ( a == 10) or (b == 10) or ((a + b) == 10):
+#        return True
+#    else:
+#        return False
+
+#print(makes10(9, 10))
+#print(makes10(9, 9))
+#print(makes10(1, 9))
+
+## Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+## near_hundred(93) → True
+## near_hundred(90) → True
+## near_hundred(89) → False
+
+def near_hundred(n):
+    if abs(100 -n)<=10 or abs(200 -n)<=10:
+        return True
+    else:
+        return False
+
+print(near_hundred(93))
+print(near_hundred(90))
+print(near_hundred(89))
+
+
+
+
+
+
+
+
+
 
