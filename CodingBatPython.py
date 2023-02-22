569ad3aea11d760fb8dd141ce2fc9eb6dfac4bc0
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-22 16:37:47 +0100

    2023.02.22

--- CodingBatPython.py
+++ CodingBatPython.py
@@ -97,16 +97,32 @@
 ## near_hundred(90) → True
 ## near_hundred(89) → False
 
-def near_hundred(n):
-    if abs(100 -n)<=10 or abs(200 -n)<=10:
+#def near_hundred(n):
+#    if abs(100 -n)<=10 or abs(200 -n)<=10:
+#        return True
+#    else:
+#        return False
+
+#print(near_hundred(93))
+#print(near_hundred(90))
+#print(near_hundred(89))
+
+## Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+## pos_neg(1, -1, False) → True
+## pos_neg(-1, 1, False) → True
+## pos_neg(-4, -5, True) → True
+
+def pos_neg(a, b, negative):
+    if (negative == True and a < 0 and b < 0) or (negative == False and a * b < 0):
         return True
     else:
         return False
 
-print(near_hundred(93))
-print(near_hundred(90))
-print(near_hundred(89))
+print(pos_neg(1, -1, False))
+print(pos_neg(-1, 1, False))
+print(pos_neg(-4, -5, True))
 
+    
 
 
 
