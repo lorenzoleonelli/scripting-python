a03ea3f6df17bbb491fce7ac63da3f7c8f48d4b9
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-22 13:10:39 +0100

    2023.02.22

--- CodingBatPython.py
+++ CodingBatPython.py
@@ -32,4 +32,32 @@
 #print(monkey_trouble(True, False))
 
 ## Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+## sum_double(1, 2) → 3
+## sum_double(3, 2) → 5
+## sum_double(2, 2) → 8
+
+#def sum_double(a, b):
+#    if a == b:
+#        return (a + b) * 2
+#    else:
+#        return ( a + b )
+
+#print(sum_double(1, 2))
+#print(sum_double(3, 2))
+#print(sum_double(2, 2))
+
+## Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+## diff21(19) → 2
+## diff21(10) → 11
+## diff21(21) → 0
+
+def diff21(n):
+    if n > 21:
+        return 2 * (n-21)
+    else:
+        return abs(n-21)
+
+print(diff21(19))
+print(diff21(10))
+print(diff21(21))      
 
