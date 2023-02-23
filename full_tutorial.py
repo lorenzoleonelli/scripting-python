3e5bd065bc16c76cf82565854fd7a8b1c5900a5a
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-23 21:03:22 +0100

    2023.02.23

--- full_tutorial.py
+++ full_tutorial.py
@@ -642,6 +642,28 @@ row3 = [" ", " ", " "]
 row2[1] = "X"
 #display(row1, row2, row3)
 
-result = input("Please enter a value: ")
-print(type(int(result)))
+#result = input("Please enter a value: ")
+#print(type(int(result)))
+
+def user_choice():
+    choice = "WRONG"
+    acceptable_range = range(0,10)
+    within_range = False
+
+    while choice.isdigit() == False or within_range == False:
+        choice = input("Please enter a number (0-10): ?")
+        if choice.isdigit() == False:
+            print("Sorry this is not a digit")
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print("Sorry, out of range")
+                within_range = False
+
+    return int(choice)
+
+user_choice()
+
+
 
