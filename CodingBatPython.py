5c8d9b7436724e35d28bcf7219cec42495896461
lorenzoleonelli <lorenzoleonelli@hotmail.com>
2023-02-23 14:11:40 +0100

    2023.02.23

--- CodingBatPython.py
+++ CodingBatPython.py
@@ -186,6 +186,30 @@
 ## Warmaup-2
 
 ## Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+## string_times('Hi', 2) → 'HiHi'
+## string_times('Hi', 3) → 'HiHiHi'
+## string_times('Hi', 1) → 'Hi'
+
+#def string_times(str, n):
+#    return str * n
+#print(string_times('Hi', 2))
+#print(string_times('Hi', 3))
+#print(string_times('Hi', 1))
+
+## Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+## front_times('Chocolate', 2) → 'ChoCho'
+## front_times('Chocolate', 3) → 'ChoChoCho'
+## front_times('Abc', 3) → 'AbcAbcAbc'
+
+#def front_times(str, n):
+#    return str[:3] * n
+# print(front_times('Chocolate', 2))
+# print(front_times('Chocolate', 3))
+# print(front_times('Abc', 3))
+
+
+
+
 
 
 
